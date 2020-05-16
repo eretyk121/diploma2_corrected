55a6f8519f7d8e4d455cad3ea487c518c351c047
eretyk121 <58330074+eretyk121@users.noreply.github.com>
2020-05-16 19:20:41 +0300

    Update test_vk.py

--- tests/test_vk.py
+++ tests/test_vk.py
@@ -7,6 +7,7 @@ class Test_Vk(unittest.TestCase):
 
     def setUp(self):
         self.user = User(TOKEN)
+        self.db_conn = ps.connect(dbname='studing', user='igor1', password='1234')
         self.target = 'soldatenkov121'
         self.list_id = [18098504, 5061261, 8232281, 228511688, 53474230, 515998263, 497652951,
                         7080060, 170748077, 179025535, 3315453, 88987002, 1876621, 406139125, 306363520, 294965239, 580804664,
@@ -30,7 +31,7 @@ class Test_Vk(unittest.TestCase):
 
 # Тест на корректность записи в базу и одновременно на отсеивание в методе sorted_people URL которые уже есть в базе.
     def test_write_data(self):
-        with  ps.connect(dbname='studing', user='igor1', password='1234') as conn:
+        with self.db_conn as conn:
             with conn.cursor() as cur:
                 count_before = 0
                 count_after = 0
